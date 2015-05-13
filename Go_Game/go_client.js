74fd4ad7674c789d1fdfafbd8da61e1c1f5edfb6
test <lucas.elliott@coloradocollege.edu>
2015-05-12 22:28:38 -0600

    Almost final

--- Go_Game/go_client.js
+++ Go_Game/go_client.js
@@ -1,6 +1,44 @@
 var player_color;
 var color = null;
 
+window.setInterval( pollBoard, 2000 );
+
+function pollBoard()
+{
+    console.log("poll board");
+    var board_req = new XMLHttpRequest();
+    board_req.onload = boardListener;
+    board_req.open( "get", "poll_board" );
+    board_req.send();
+}
+
+function boardListener(){
+  var new_positions = JSON.parse( this.responseText );
+  var board = document.getElementById( "board" );
+  console.log("listening");
+  var pos_y = 1;
+
+  for( var y = 1; y < 10; y=y+2 ){
+    for( var x = 0; x < 5; x++ )
+        {
+            //console.log( board.childNode);
+            var tr_elem = board.childNodes[y];
+            //console.log( tr_elem + y);
+            var td_elem = tr_elem.childNodes[x];
+            //console.log( td_elem );
+            var dv_elem = td_elem.childNodes[0];
+            //console.log( dv_elem );
+            if( new_positions[pos_y][x] == "-" ) {
+              dv_elem.innerHTML = "+";
+            }
+            else{
+              dv_elem.innerHTML = new_positions[pos_y][x];
+            }
+        }
+        pos_y++;
+  }
+}
+
 function playerSelection(){
   console.log( "select" );
 
@@ -33,4 +71,12 @@ function gameStatus(){
   else if( color == "obs" ){
     game_status.innerHTML = "There's currently a game in progess. Feel free to watch.";
   }
+}
+
+function boardClick( grid ){
+  console.log( grid.getAttribute( "position" ) );
+
+  var sendgrid = new XMLHttpRequest();
+  sendgrid.open( "get", color + "?grid=" + grid.getAttribute( "position" ) );
+  sendgrid.send();
 }
--- Go_Game/go_server.js
+++ Go_Game/go_server.js
@@ -1,7 +1,44 @@
 var http = require( "http" );
 var fs = require( "fs" );
+var args = process.argv;
 
 var players = 0;
+var pieces;
+var move = "black";
+
+setBoard();
+
+function setBoard(){
+  pieces = fs.readFileSync( args[2] ).toString().split( "\n" );
+
+  for ( var i = 0; i < pieces.length; i++ ){
+    pieces[i] = pieces[i].split( "" );
+  }
+  console.log(pieces)
+
+}
+
+function setPiece( req, res ) {
+  var pieceColor = req.url.split( "?" )[0].substring( 1, 6 );
+  var y_coor = parseInt( req.url.split( "?" )[1].split( "=" )[1].split( "," )[0] );
+  var x_coor = parseInt( req.url.split( "?" )[1].split( "=" )[1].split( "," )[1] );
+
+  console.log( pieceColor + y_coor + x_coor );
+
+  if( pieceColor == move ){
+    pieces[y_coor+1][x_coor] = pieceColor.substring( 0, 1 );
+    console.log(move);
+    console.log(pieces);
+
+    if( move == "black" ){
+      move = "white";
+    }
+    else{
+      move = "black";
+    }
+    console.log( move );
+  }
+}
 
 function playerAssignment( req, res ){
   console.log( "here in server" );
@@ -38,11 +75,26 @@ function serveFile( filename, req, res )
     res.end( contents );
 }
 
+function sendBoard( req, res ){
+  console.log("send board")
+  res.writeHead( 200 );
+  res.end( JSON.stringify(pieces) );
+}
+
 function serverFn( req, res ){
-  console.log( "getting file" )
+
   var filename = req.url.substring( 1, req.url.length );
+
+  console.log( filename );
+
   if( filename.substring( 0, 17 ) == "player_assignment" ){
     playerAssignment( req, res );
+  }
+  else if( filename.substring( 0, 5 ) == "black" || filename.substring( 0, 5 ) == "white" ){
+    setPiece( req, res );
+  }
+  else if (filename == "poll_board" ){
+    sendBoard( req, res );
   }
   else if( filename == "go_client.js" ){
     serveFile( "go_client.js", req, res );
